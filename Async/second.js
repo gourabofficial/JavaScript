a550708d099ca0370db5b63b61635423d8df1c85
gourabofficial <gangulygourab3@gmail.com>
2024-08-27 08:11:18 +0530

    chore: Add asynchronous data retrieval function

---
+++ Async/second.js
@@ -0,0 +1,18 @@
+function getData(dataId , NextData) {
+    setTimeout(() => {
+        console.log("data :", dataId);
+        if (NextData) {
+            NextData()
+        }
+    },2000)
+}
+getData(9, () => {
+    getData(10, () => {
+        getData(11, () => {
+            getData(12, () => {
+           
+            });
+        });
+        
+    });
+});
