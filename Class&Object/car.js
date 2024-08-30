ed9ea9a070e46391bf297c3412370f79dbdd4a0a
gourabofficial <gangulygourab3@gmail.com>
2024-08-30 09:09:30 +0530

    class in js

---
+++ Class&Object/car.js
@@ -0,0 +1,14 @@
+class car {
+    start() {
+        console.log("start the car ...");
+        
+    }
+    stop() {
+        console.log("stop the car ...");
+        
+    }
+}
+
+let mycar = new car();
+mycar.start();
+mycar.stop();
