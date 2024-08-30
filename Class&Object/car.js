53ba115522f9365a332b7b80a201d6242ce8a579
gourabofficial <gangulygourab3@gmail.com>
2024-08-30 10:24:59 +0530

    class ..

--- Class&Object/car.js
+++ Class&Object/car.js
@@ -1,14 +1,19 @@
 class car {
     start() {
         console.log("start the car ...");
-        
     }
     stop() {
         console.log("stop the car ...");
+    }
+    setBrandName(brand) {
+        this.brandname = brand;
         
     }
+    
 }
 
 let mycar = new car();
+// let maruti = new car();
+mycar.setBrandName("MARUTI");
 mycar.start();
 mycar.stop();
