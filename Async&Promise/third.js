74264b3571bd6bc6889167bc0750083b3603c1a3
gourabofficial <gangulygourab3@gmail.com>
2024-08-31 09:05:03 +0530

    learning promise

--- Async&Promise/third.js
+++ Async&Promise/third.js
@@ -1,11 +1,29 @@
-function getData(dataId, getNextData) {
-    return new Promise((resolve , reject) => {
-        setTimeout(() => {
-            console.log('Data:' ,dataId);
-            resolve("succes....")
-            if(getNextData) {
-                getNextData();
-            }
-        },4000)
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("I am a promise");
+         resolve("succes...")
+        // reject("error!")
     })
-}
+};
+
+let promise = getPromise();
+promise.then((res) => {
+    console.log("promose fulfild..",res)
+});
+
+promise.catch((rej) => {
+    console.log("promise rejected..",rej);
+
+});
+
+// function getData(dataId, getNextData) {
+//     return new Promise((resolve , reject) => {
+//         setTimeout(() => {
+//             console.log('Data:' ,dataId);
+//             resolve("succes....")
+//             if(getNextData) {
+//                 getNextData();
+//             }
+//         },4000)
+//     })
+// }
