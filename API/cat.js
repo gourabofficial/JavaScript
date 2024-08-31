4a26326cb05c5e9be8dff8712e8126de861ba7fe
gourabofficial <gangulygourab3@gmail.com>
2024-08-31 20:08:02 +0530

    Introduction to api

--- API/cat.js
+++ API/cat.js
@@ -1,7 +1,13 @@
 let url = "https://cat-fact.herokuapp.com/facts";
+let fact = document.querySelector("#fact");
+let btn = document.querySelector("#btn");
 
 const getFact = async () => {
     console.log("Fetching data...");
     let response = await fetch(url);
-    console.log(response);
+    console.log(response); // json format 
+    let data = await response.json();
+    fact.innerText = data[2].text;
+   
 }
+btn.addEventListener("click", getFact);
