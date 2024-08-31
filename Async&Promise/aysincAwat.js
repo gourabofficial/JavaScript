49b61207b382ff789af243b3dcd83043a8ab0c9e
gourabofficial <gangulygourab3@gmail.com>
2024-08-31 11:35:26 +0530

    aysinc and Awat..

---
+++ Async&Promise/aysincAwat.js
@@ -0,0 +1,22 @@
+async function getData(dataId) {
+    return new Promise((resolve, reject) => {
+        console.log('Data: ', dataId);
+        setTimeout(() => {
+            resolve('success');
+        }, 3000); 
+    });
+}
+
+
+//aysinc await
+ 
+async function fetchData() {
+    console.log("Fetching data...");
+    await getData(1);
+    console.log("Fetching data...");
+    await getData(2);
+    console.log("Fetching data...");
+    await getData(3);
+    console.log("Fetching data...");
+    await getData(4);
+}
