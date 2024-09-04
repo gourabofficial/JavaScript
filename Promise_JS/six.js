413d96cc6890e83275c8f0506c13e03236fb5006
gourabofficial <gangulygourab3@gmail.com>
2024-09-04 23:41:29 +0530

    all in one file

--- Promise_JS/six.js
+++ Promise_JS/six.js
@@ -39,11 +39,29 @@ const promiseSeven = new Promise((resolve, reject) => {
     });
 });
 
-promiseSeven.then((user) => {
-    console.log(user);
-    console.log(user.username);
-    
-}).catch((error) => {
-    console.log(error);
-    
-})
+promiseSeven
+    .then((user) => {
+        console.log(user);
+        console.log(user.username);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log("all are working fine");
+    })
+
+
+async function Getinfo() {
+    try {
+        const response1 = await fetch('https://api.github.com/users/BuddhadebKoner');
+        const response2 = await fetch('https://api.github.com/users/gourabofficial');
+        const data1 = await response1.json();
+        const data2 = await response2.json();
+        console.log(data1);
+        console.log(data2);
+    } catch (error) {
+        console.log(error);
+    }
+}
+Getinfo();
