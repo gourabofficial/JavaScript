ad2f7cefda127b159e87cee7c8a138c9dac15634
gourabofficial <gangulygourab3@gmail.com>
2024-09-05 20:39:52 +0530

    opps exploring

---
+++ Class&Object/information.js
@@ -0,0 +1,16 @@
+function information(username, passward, isLoogdin) {
+    this.username = username;
+    this.passward = passward;
+    this.isLoogdin = isLoogdin;
+    
+   this.Printinfo =  function () {
+        console.log(`Welcome ${this.username}`)
+    }
+}
+
+const userOne = new information("Gourab", 1234, true);
+const userTwo = new information("Rana", 2222, false);
+console.log(userOne)
+userOne.Printinfo()
+
+// console.log(userTwo)
--- Promise_JS/six.js
+++ Promise_JS/six.js
@@ -92,11 +92,11 @@ promiseGetinfo.then(data => {
 })
 
 
-// fetch('https://api.github.com/users/hiteshchoudhary')
-// .then((response) => {
-//     return response.json()
-// })
-// .then((data) => {
-//     console.log(data);
-// })
-// .catch((error) => console.log(error))
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
