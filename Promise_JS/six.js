49c715df79003243ecb96a781ea0b4dd9a998ad5
gourabofficial <gangulygourab3@gmail.com>
2024-09-05 19:03:08 +0530

    all in one function

--- Promise_JS/six.js
+++ Promise_JS/six.js
@@ -90,3 +90,13 @@ promiseGetinfo.then(data => {
 }).catch(error=>{
     console.log(error)
 })
+
+
+// fetch('https://api.github.com/users/hiteshchoudhary')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => console.log(error))
