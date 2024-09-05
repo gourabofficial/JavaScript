dbff9e5f8e946d5d7463c6b07b34d3de8e0e41f5
gourabofficial <gangulygourab3@gmail.com>
2024-09-06 00:30:35 +0530

    .call in js

--- Class&Object/call.js
+++ Class&Object/call.js
@@ -1,13 +1,17 @@
 function SetUsername(username) {
-    this.username = username;
+    if (username == "rana" ) {
+       this.username = username
+   }
 }
 
 const testCalling = function (username, password, email) {
     SetUsername.call(this,username); // .call this is the syntex
     this.password = password;
     this.email = email; 
-
+ 
 }
 
 const printData = new testCalling("gourab", 1234, "gg@fb.com");
+const printData2 = new testCalling("rana", 1234, "gg@fb.com");
 console.log(printData)
+console.log(printData2)
