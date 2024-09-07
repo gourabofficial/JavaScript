428c8cb84e2719230a496267f8bcd4535ba75827
gourabofficial <gangulygourab3@gmail.com>
2024-09-07 16:13:55 +0530

    Refactor password setter in User class to validate password length before setting it

--- Class&Object/GettersAndSetters.js
+++ Class&Object/GettersAndSetters.js
@@ -8,13 +8,21 @@ class User  {
         return this.pass;
     }
     set password(value) {
-         this.pass = value.toUpperCase();
+        if (value.length < 6) {
+            console.log("Password is too short");
+           
+        } else {
+            console.log("Password is set");
+            this.pass = value.toUpperCase();
+        }
+         
     }
 }
 
-const user1 = new User("Gourab ganguly", "gourab@abc", 1234);
-console.log(user1.password);// 1234
-user1.password = "@5432hn"
+const user1 = new User();
+// console.log(user1.password);// 1234
+// user1.password = "@5hn"
+user1.password = "@5hnas"
 console.log(user1.password); 
 
 
