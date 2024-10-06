112c131a3c3a5e76bcd0c13032294328b6eb2831
gourabofficial <gangulygourab3@gmail.com>
2024-10-06 13:55:40 +0530

    question

--- Arrays/test.js
+++ Arrays/test.js
@@ -1,11 +1,27 @@
-const arr = [0, 1, 3, 0, 1, 1, 2, 1, 0, 0, 1, 0];
-let count = 0;
+const arr = [0, 1, 3, 0, 26, 99, 77, 88, 8, 10, 1, 1, 2, 1, 0, 0, 1, 0];
+arr.sort();
+console.log(arr);
 
+const target_element = 26;
+binary_search(arr, target_element);
+
+
+
+function binary_search(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let mid = (right - left / 2);
     
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
-        count++;
+    while (left <= right) {
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
+    return -1;
 
 }
-console.log(count);
+
